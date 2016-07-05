047ea99dd862f23389807e9fcdf09fbfd52e60c2
Isak Sylvin <isak.sylvin@scilifelab.se>
2016-07-05 15:02:20 +0200

    Made password (thus backuping up private repos) optional

--- backup_github.py
+++ backup_github.py
@@ -38,8 +38,14 @@ class cd(object):
 def backup(user, password, dest):
     """Performs a backup of all the public repos in user's GitHub account on dest
     """
-    gh = Github(login=user, user=user, password=password)
-    repos = gh.repos.list(type='all')
+    import pdb
+    pdb.set_trace()
+    if not password is None:
+        gh = Github(login=user, user=user, password=password)
+        repos = gh.repos.list(type='all')
+    else:
+        gh = Github()
+        repos = gh.repos.list(type='all', user=user)
     for repo in repos.all():
         repo_path = os.path.join(dest, repo.name)
         LOG.info("Backing up repository {}".format(repo.name))
@@ -72,7 +78,7 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Clones all the public " \
             "repositories from a GitHub account")
     parser.add_argument("user", type=str, help="GitHub username")
-    parser.add_argument("password", type=str, help="GitHub password")
+    parser.add_argument("password", nargs='?', type=str, help="GitHub password")
     parser.add_argument("-d", type=str, help="Destination of the copy")
     args = parser.parse_args()
 
