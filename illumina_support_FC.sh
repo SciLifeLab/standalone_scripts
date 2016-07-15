f12d775b01abbe115adad8d4bb0eaec4812ec103
Francesco Vezzi <vezzi84@gmail.com>
2016-07-15 15:16:57 +0200

    geenralised script to download illumina info to be sent to illumina

--- illumina_support_FC.sh
+++ illumina_support_FC.sh
@@ -4,13 +4,16 @@
 ###
 
 
-if [ $# -eq 1 ]; then
-    echo "Trying to retrive information for FC $0"
+if [ $# -eq 3 ]; then
+    echo "Trying to retrive information for FC $1 from server $2 from user $3"
 else
-    echo "You need to specify one FC (DATE_INSTRUM_RUN_FCID)"
+    echo "You need to specify one FC (DATE_INSTRUM_RUN_FCID), a server (preproc1@scilifelab.se), and a user name (name)"
 fi
 
 FC="$1"
+server="$2"
+user="$3"
+
 
 
 if [ -d $FC ]; then
@@ -21,32 +24,32 @@ fi
 mkdir $FC
 
 echo "  fetch xml info"
-scp -r francesco.vezzi@preproc1.scilifelab.se:/srv/illumina/HiSeq_X_data/nosync/$FC/*xml $FC > $FC\_sync.out 2> $FC\_sync.err
+echo scp -r $user@$server:/srv/illumina/HiSeq_X_data/nosync/$FC/*xml $FC > $FC\_sync.out 2> $FC\_sync.err
 
 echo "   fetch csv info"
-scp -r francesco.vezzi@preproc1.scilifelab.se:/srv/illumina/HiSeq_X_data/nosync/$FC/*csv $FC  >> $FC\_sync.out 2>> $FC\_sync.err
+scp -r $user@$server:/srv/illumina/HiSeq_X_data/nosync/$FC/*csv $FC  >> $FC\_sync.out 2>> $FC\_sync.err
 
 echo "   fetch std out and std err of commands (bcl2fastq and rsync)"
-scp -r francesco.vezzi@preproc1.scilifelab.se:/srv/illumina/HiSeq_X_data/nosync/$FC/*.err $FC >> $FC\_sync.out 2>> $FC\_sync.err
-scp -r francesco.vezzi@preproc1.scilifelab.se:/srv/illumina/HiSeq_X_data/nosync/$FC/*.out $FC >> $FC\_sync.out 2>> $FC\_sync.err
+scp -r $user@$server:/srv/illumina/HiSeq_X_data/nosync/$FC/*.err $FC >> $FC\_sync.out 2>> $FC\_sync.err
+scp -r $user@$server:/srv/illumina/HiSeq_X_data/nosync/$FC/*.out $FC >> $FC\_sync.out 2>> $FC\_sync.err
 
 echo "   fetch Logs dir"
-scp -r francesco.vezzi@preproc1.scilifelab.se:/srv/illumina/HiSeq_X_data/nosync/$FC/Logs $FC >> $FC\_sync.out 2>> $FC\_sync.err
+scp -r $user@$server:/srv/illumina/HiSeq_X_data/nosync/$FC/Logs $FC >> $FC\_sync.out 2>> $FC\_sync.err
 
 echo "    fetch InterOp dir"
-scp -r francesco.vezzi@preproc1.scilifelab.se:/srv/illumina/HiSeq_X_data/nosync/$FC/InterOp $FC >> $FC\_sync.out 2>> $FC\_sync.err
+scp -r $user@$server:/srv/illumina/HiSeq_X_data/nosync/$FC/InterOp $FC >> $FC\_sync.out 2>> $FC\_sync.err
 
 echo "    fetch Config dir"
-scp -r francesco.vezzi@preproc1.scilifelab.se:/srv/illumina/HiSeq_X_data/nosync/$FC/Config $FC >> $FC\_sync.out 2>> $FC\_sync.err
+scp -r $user@$server:/srv/illumina/HiSeq_X_data/nosync/$FC/Config $FC >> $FC\_sync.out 2>> $FC\_sync.err
 
 echo "    fetch RTALogs dir"
-scp -r francesco.vezzi@preproc1.scilifelab.se:/srv/illumina/HiSeq_X_data/nosync/$FC/RTALogs $FC >> $FC\_sync.out 2>> $FC\_sync.err
+scp -r $user@$server:/srv/illumina/HiSeq_X_data/nosync/$FC/RTALogs $FC >> $FC\_sync.out 2>> $FC\_sync.err
 
 echo "    fetch Recipe dir"
-scp -r francesco.vezzi@preproc1.scilifelab.se:/srv/illumina/HiSeq_X_data/nosync/$FC/Recipe $FC >> $FC\_sync.out 2>> $FC\_sync.err
+scp -r $user@$server:/srv/illumina/HiSeq_X_data/nosync/$FC/Recipe $FC >> $FC\_sync.out 2>> $FC\_sync.err
 
 echo "    fetch Demux dir"
-scp -r francesco.vezzi@preproc1.scilifelab.se:/srv/illumina/HiSeq_X_data/nosync/$FC/Demultiplexing $FC >> $FC\_sync.out 2>> $FC\_sync.err
+scp -r $user@$server:/srv/illumina/HiSeq_X_data/nosync/$FC/Demultiplexing $FC >> $FC\_sync.out 2>> $FC\_sync.err
 
 tar -zcvf $FC.tar.gz $FC >> $FC\_sync.out 2>> $FC\_sync.err
 
