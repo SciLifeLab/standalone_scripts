60f0364a0800e50cfaba23891a1b64f06e0aab95
Kate Stepanova <ekaterina.stepanova@scilifelab.se>
2016-10-21 15:45:59 +0200

    Project status improvements

--- project_status.py
+++ project_status.py
@@ -1,7 +1,7 @@
 import sys, os, glob
 import argparse
 from operator import itemgetter
-import glob
+import subprocess
 
 def init_sample_hash_emtry():
     empty_sample_result = {
@@ -216,9 +216,6 @@ def parse_qualimap(genome_results_file, sample):
     sample['AutosomalCoverage'] = autosomal_cov_bases / autosomal_cov_length
 
 
-
-
-
 def main(args):
     uppmax_id    = args.uppmax_project
     stockholm    = args.stockholm
@@ -229,14 +226,14 @@ def main(args):
     samples      = {}
 
 
-    projects     = [item for sublist in args.projects for item in sublist]
+    projects     = args.projects #[item for sublist in args.projects for item in sublist]
 
     if args.project_status:
-        if len(args.projects[0]) != 1:
+        if len(projects) != 1:
             print "WARNING: only one project when project-status specified\n"
             return
 
-    for project in args.projects[0]:
+    for project in projects:
         #find all samples sequenced for a project present in archive -- this assumes that fastq files will be deleted but not the folder structure
         find_samples_from_archive(archive, project, samples, stockholm)
         #now find samples that are stored in DATA
@@ -298,25 +295,355 @@ def main(args):
                     sample_entry['MedianInsertSize']
                 )
 
+def get_low_coverage(project):
+    # run same script again, to parse the output
+    # because all the functions above don't return anything, but just print the result
+    script_path = os.path.realpath(__file__)
+    command = """python {} {} | awk -v x=28.5 '$7<x' | awk '{{ print $1 " " $7}}' """.format(script_path, project)
+    try:
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    except Exception, e:
+        print "Command failed: {}".format(command)
+        raise e
+    output = p.communicate()[0]
+
+    result = {}
+    for line in output.split('\n'):
+        try:
+            sample, coverage = line.split()
+        except:
+            continue
+        else:
+            result[sample] = coverage
+    return result
+
+def get_project_status(project):
+    # run same script again, to parse the output
+    # because all the functions above don't return anything, but just print the result
+    script_path = os.path.realpath(__file__)
+    command = """python {} {} --project-status --skip-header""".format(script_path, project)
+    try:
+        p = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except Exception, e:
+        print "Command failed: {}".format(command)
+        raise e
+    output, error = p.communicate()
+    if error:
+        print error
+        exit(1)
+
+    result = {
+        'sequenced': [],
+        'resequenced': [],
+        'organized': [],
+        'not_organized': [],
+        'analyzed': [],
+        'to_analyze': [],
+    }
+
+    for line in output.split('\n'):
+        try:
+            sample, sequenced, organized, analysed = line.split()
+            if project not in sample:
+                continue
+        except Exception,e: continue
+        result['sequenced'].append(sample)
+        if sequenced > '1':
+            result['resequenced'].append(sample)
+        if organized != '0':
+            result['organized'].append(sample)
+        if organized < sequenced:
+            result['not_organized'].append(sample)
+        if analysed != '0':
+            result['analyzed'].append(sample)
+        if analysed < sequenced:
+            result['to_analyze'].append(sample)
+    return result
+
+def get_samples_with_undetermined(data_dir, project):
+    """ get all fastq_files from DATA directory
+        check which ones named 'Undetermined'
+        then add sample and flowcell to the list
+    """
+    result = {}
+    # get list of fastq_files in DATA directory
+    project_path = os.path.join(data_dir, project, '*/*/*/*.fastq*')
+    fastq_files = glob.glob(project_path)
+    for file_path in fastq_files:
+        # check which files are named 'Undetermined'
+        filename = os.path.basename(file_path)
+        if 'Undetermined' in filename:
+            # get sample and flowcell id
+            sample = filename.split('_Undetermined')[0]
+            flowcell = file_path.split('/')[-2]
+            # update result list
+            if sample not in result:
+                result[sample] = [flowcell]
+            elif flowcell not in result[sample]:
+                result[sample].append(flowcell)
+    return result
+
+def get_samples_under_analysis(project):
+    result = []
+    command = "jobinfo | grep piper | grep {}".format(project)
+    try:
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = p.communicate()
+    except Exception, e:
+        print "Cannot execute command: {}".format(command)
+        raise e
+
+    output = output[0].split('\n')
+    for line in output:
+        # skip empty lines
+        if line.strip() != '':
+            sample = line.split('piper_{}-'.format(project))[-1].split('-')[0]
+            if sample not in result:
+                result.append(sample)
+    return result
+
+def get_samples_under_qc(project):
+    result = []
+    command = "jobinfo | grep qc_{}".format(project)
+    try:
+        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = p.communicate()
+    except Exception, e:
+        print "Cannot execute command {}".format(command)
+        raise e
+    output = output[0].split('\n')
+    for line in output:
+        # skip empty lines
+        if line.strip() != '':
+            sample = line.split('qc_{}-'.format(project))[-1]
+            if sample not in result:
+                result.append(sample)
+    return result
+
+def get_samples_with_failed_analysis(project, analysis_dir):
+    log_path = os.path.join(analysis_dir, '{}/piper_ngi/logs/{}-*.exit'.format(project, project))
+    under_analysis = get_samples_under_analysis(project)
+    exit_files = glob.glob(log_path)
+    result = {}
+    for path in exit_files:
+        with open(path, 'r') as exit_file:
+            exit_code = exit_file.read().strip()
+            # P4603-P4603_189-merge_process_variantcall.exit
+            sample = os.path.basename(path).replace('{}-'.format(project), '').split('-')[0]
+            if exit_code == '' and sample not in under_analysis:
+                if sample in result:
+                    result[sample] = [result[sample]]
+                    result[sample].append('Empty exit code, but sample is not under analysis')
+                else:
+                    result[sample] = 'Empty exit code, but sample is not under analysis'
+            elif exit_code.strip() != '0' and exit_code != '':
+                if sample in result:
+                    result[sample] = [result[sample]]
+                    result[sample].append('Exit code: {}'.format(exit_code))
+                else:
+                    result[sample] = 'Exit code: {}'.format(exit_code)
+    return result
+
+def get_incoherent_samples(project):
+
+    # run same script again, to parse the output
+    # because all the functions above don't return anything, but just print the result
+    script_path = os.path.realpath(__file__)
+    command = """python {} {} --project-status --skip-header""".format(script_path, project)
+    try:
+        p = subprocess.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except Exception, e:
+        print "Command failed: {}".format(command)
+        raise e
+    output, error = p.communicate()
+    if error:
+        print error
+        exit(1)
+    result = {}
+    for line in output.split('\n'):
+        try:
+            sample, sequenced, organized, analyzed = line.split()
+        except Exception, e:
+            continue
+        # skipping header
+        if sample == "SAMPLE":
+            continue
+        if sequenced != organized or organized != analyzed:
+            result[sample] = [sequenced, organized, analyzed]
+    return result
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("""Process one or more project and report basic statistiscs for it """)
-    parser.add_argument('--projects', help="Projects we want to have statistics for, in stockholm case P1000, uppsala NK-0191", type=str,   action='append', nargs='+')
-    parser.add_argument('--uppmax-project', help="uppmax project where analysis have been run", type=str, required=True)
+    parser.add_argument('projects', metavar='project', type=str, nargs='+', help='Projects we want to have statistics for, in stockholm case P1000, uppsala NK-0191')
+    parser.add_argument('--uppmax-project', help="uppmax project where analysis have been run", type=str, default='ngi2016003')
     parser.add_argument('--project-status', help="reports number of samples, of samples-runs, analysed samples and delivered samples (work only if a single project is specified)", action='store_true')
 
     parser.add_argument('--skip-header', help="skip header", action='store_true')
     parser.add_argument('--stockholm', help="assume stocholm project format, otherwise uppsala", action='store_true', default=True)
 
-    args = parser.parse_args()
+    # added by Kate
+    parser.add_argument('--sequenced', help="List of all the sequenced samples", action="store_true")
+    parser.add_argument('--low-coverage', help="List of analyzed samples with coverage below 28.5X", action="store_true")
+    parser.add_argument('--organized', help="List of all the organized flowcells", action="store_true")
+    parser.add_argument('--not-organized', help="List of all the not-organized flowcells", action="store_true")
+    parser.add_argument('--analyzed', help="List of all the analysed samples", action="store_true")
+    parser.add_argument('--resequenced', help="List of samples that have been sequenced more than once", action="store_true")
+    parser.add_argument('--undetermined', help="List of the samples which use undetermined", action="store_true")
+    parser.add_argument('--under-analysis', help="List of the samples under analysis", action="store_true")
+    parser.add_argument('--to-analyze', help="List of samples that are ready to be analyzed", action="store_true")
+    parser.add_argument('--under-qc', help="List of samples under qc. Use only for projects without BP", action="store_true")
+    parser.add_argument('--analysis-failed', help="List of all the samples with failed analysis", action="store_true")
+    parser.add_argument('--short', help="Project-status but only for samples which have incoherent number of sequenced/organized/analyzed", action="store_true")
 
+    # todo
+    parser.add_argument('--all-samples', help="List of all the samples of project (sequenced and not sequenced). Not implemented yet", action="store_true")
+    parser.add_argument('--not-sequenced', help="List of the samples that are not sequenced AT ALL on ANY flowcells or lanes. Not implemented yet", action="store_true")
+    parser.add_argument('--qc-done', help="List of samples with completed QC. Not implemented yet", action="store_true")
+    parser.add_argument('--sample', '-s', help="Statistics for the specified sample. Not implemented yet", type=str)
+
+    args = parser.parse_args()
     if not args.projects:
         print "ERROR: projects must be specified"
         sys.exit()
 
-    main(args)
+    # parse arguments
+    project = args.projects[0]
+    uppmax_id = args.uppmax_project
+    data_dir = "/proj/{}/nobackup/NGI/DATA/".format(uppmax_id)
+    analysis_dir = "/proj/{}/nobackup/NGI/ANALYSIS/".format(uppmax_id)
 
+    # output the result
+    if args.low_coverage:
+        samples = get_low_coverage(project)
+        if samples:
+            if not args.skip_header:
+                print "Coverage below 28.5X:"
+            for sample in sorted(samples.keys()):
+                print "{}   {}".format(sample, samples[sample])
+        else:
+            print 'All samples are above 28.5X'
 
+    elif args.resequenced:
+        samples = get_project_status(project)
+        if samples['resequenced']:
+            if not args.skip_header:
+                print 'Resequenced samples:' # todo: add flowcells
+            for sample in sorted(samples['resequenced']):
+                print sample
+        else:
+            print 'No resequenced samples'
 
+    elif args.sequenced:
+        samples = get_project_status(project)
+        if samples['sequenced']:
+            if not args.skip_header:
+                print 'Sequenced samples:'
+            for sample in sorted(samples['sequenced']):
+                print sample
+        else:
+            print 'No sequenced samples'
+
+    elif args.organized:
+        samples = get_project_status(project)
+        if samples['organized']:
+            if not args.skip_header:
+                print 'Organized samples:'
+            for sample in sorted(samples['organized']):
+                print sample
+        else:
+            print 'No organized samples'
+
+    elif args.not_organized:
+        samples = get_project_status(project)
+        if samples['not_organized']:
+            if not args.skip_header:
+                print 'Not organized samples:'
+            for sample in sorted(samples['not_organized']):
+                print sample # todo: add flowcell list
+        else:
+            print 'All samples organized'
+
+    elif args.analyzed:
+        samples = get_project_status(project)
+        if samples['analyzed']:
+            if not args.skip_header:
+                print 'Analyzed samples:'
+            for sample in sorted(samples['analyzed']):
+                print sample
+        else:
+            print 'No analyzed samples'
+
+    elif args.undetermined:
+        samples = get_project_status(project)
+        result = get_samples_with_undetermined(data_dir, project)
+        if result:
+            if not args.skip_header:
+                print 'Organized with undetermined:'
+            for sample in sorted(result.keys()):
+                print "{}: {}".format(sample, ", ".join(fc for fc in result[sample]))
+        else:
+            print 'No undetermined used'
+
+    elif args.under_analysis:
+        result = get_samples_under_analysis(project)
+        if result:
+            if not args.skip_header:
+                print 'Samples under analysis:'
+            for sample in sorted(result):
+                print sample
+        else:
+            'No samples are being analyzed'
+
+    elif args.to_analyze:
+        samples = get_project_status(project)
+        to_analyze = samples['to_analyze']
+        if to_analyze:
+            print 'something'
+            if not args.skip_header:
+                print 'Samples ready to be analyzed:'
+            for sample in sorted(to_analyze):
+                print sample
+        else:
+            print 'No samples ready to be analyzed'
+
+    elif args.all_samples:
+        print "--all-samples has not been implemented yet"
+
+    elif args.not_sequenced:
+        print "--not-sequenced has not been implemented yet"
+
+    elif args.under_qc:
+        result = get_samples_under_qc(project)
+        if result:
+            if not args.skip_header:
+                print 'Samples under QC:'
+            for sample in sorted(result):
+                print sample
+        else:
+            print 'No samples under QC'
+
+    elif args.analysis_failed:
+        result = get_samples_with_failed_analysis(project, analysis_dir)
+        if result:
+            if not args.skip_header:
+                print 'Samples with failed analysis:'
+            for sample in sorted(result):
+                print sample, result[sample]
+        else:
+            print 'No analysis failed'
+
+    elif args.short:
+        result = get_incoherent_samples(project)
+        if result:
+            if not args.skip_header:
+                print "Samples with incoherent runs:"
+            for sample in sorted(result.keys()):
+                numbers = result[sample]
+                print "{}\t{}\t{}\t{}".format(sample, numbers[0], numbers[1], numbers[2])
+        else:
+            print "No samples with incoherent runs"
+
+    else:
+        main(args)
