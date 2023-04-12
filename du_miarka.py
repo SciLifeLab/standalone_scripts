676c32646ff1f1d0d8f5e5b7c81240f4ef1404b2
Johannes Alneberg <alneberg@kth.se>
2023-04-12 10:55:01 +0200

    Cleanup and more comments to du_miarka

--- du_miarka.py
+++ du_miarka.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""A faster version of `du` for our cluster Miarka, using ceph supplied space consumption.
+
+Author: @alneberg
+"""
+
 
 import argparse
 import glob
@@ -19,37 +24,40 @@ def sizeof_fmt(num, suffix="B"):
     return f"{num:.1f}Yi{suffix}"
 
 
-def print_file_size(path, bytes, non_human_readable=False):
-    if non_human_readable:
+def print_file_size(path, bytes, raw_bytes=False):
+    if raw_bytes:
         print(f"{bytes}\t{path}")
     else:
         print(f"{sizeof_fmt(bytes)}\t{path}")
 
 
-def main(input_paths, non_human_readable=False, size_sorted=False, depth=0):
+def main(input_paths, raw_bytes=False, size_sorted=False, depth=0):
     paths = []
-    new_paths = []
+    # Do the depth traversing
     for path in input_paths:
         paths.append(path)
         for depth in range(depth):
-            path += '/*'
+            path += "/*"
             depth_paths = glob.glob(path)
             if not depth_paths:
-                break
+                break  # reached the tip of the branch
             paths += depth_paths
 
+    # Append files and sizes to a list so that it can be sorted in the end
     filesizes = []
     for path in paths:
         # Check if path is not a directory
         if not os.path.isdir(path):
+            # getfattr didn't work for files, so we use os.stat
             try:
                 statinfo = os.stat(path)
             except OSError as e:
+                # This happens ƒor example with broken links
                 print(str(e), file=sys.stderr)
             bytes = statinfo.st_size
             filesizes.append((path, bytes))
-        else:  # Directory
-            # Requires python 3.5 or higher
+        else:
+            # .run Requires python 3.5 or higher
             result = subprocess.run(
                 ["getfattr", "-n", "ceph.dir.rbytes", path],
                 stdout=subprocess.PIPE,
@@ -58,12 +66,19 @@ def main(input_paths, non_human_readable=False, size_sorted=False, depth=0):
             if result.returncode != 0:
                 print(result.stderr.decode("utf-8"), file=sys.stderr)
                 continue
+
+            # Typical output from getfattr:
+            #
+            #  # file: proj/ngi2016004/private/johannes/pipelines
+            #  ceph.dir.rbytes="3699513252"
+            #
+
             lines = result.stdout.decode("utf-8").splitlines()
             for line in lines:
                 if line.startswith("# file:"):
                     filename = line.split(" ")[2]
                     filename = filename.strip()
-                if line.startswith("ceph.dir.rbytes"):
+                elif line.startswith("ceph.dir.rbytes"):
                     bytes = int((line.split("=")[1]).replace('"', ""))
                     filesizes.append((filename, bytes))
                     bytes_readable = sizeof_fmt(bytes)
@@ -72,17 +87,17 @@ def main(input_paths, non_human_readable=False, size_sorted=False, depth=0):
         filesizes.sort(key=lambda x: x[1], reverse=False)
 
     for filename, bytes in filesizes:
-        print_file_size(filename, bytes, non_human_readable)
+        print_file_size(filename, bytes, raw_bytes)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="A faster version of `du` for our cluster Miarka, using ceph supplied space consumption."
     )
-    parser.add_argument("paths", nargs="*", help="The path that should be looked into")
+    parser.add_argument("paths", nargs="*", help="The paths that should be looked into")
     parser.add_argument(
-        "-n",
-        "--non-human-readable",
+        "-r",
+        "--raw-bytes",
         action="store_true",
         help="Print sizes in bytes instead of human readable format (e.g. 1K 234M 2G)",
     )
@@ -96,4 +111,4 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    main(args.paths, args.non_human_readable, args.sort, args.depth)
+    main(args.paths, args.raw_bytes, args.sort, args.depth)
