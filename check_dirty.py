8cc38094852cd0b13a7087c9812b3de882e8abdb
Alma Hansen <alma.hansen@scilifelab.se>
2023-09-28 09:20:44 +0200

    added check_dirty.py to check if a repo is dirty

--- check_dirty.py
+++ check_dirty.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """
 Checks if a repo is dirty.
 """
@@ -5,14 +6,14 @@ import argparse
 import sys
 from git import Repo
 
-def check_if_dirty(repository):
-	repo = Repo(args.repo_path)
-	if repo.is_dirty():
-		print("Repo is dirty.", file=sys.stderr)
+def check_if_dirty(repo_path):
+    repo = Repo(repo_path)
+    if repo.is_dirty():
+	    print("Repo is dirty.", file=sys.stderr)
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='check if dirty')
-	parser.add_argument('repo_path', metavar='P', type=str, help='path to repository to check')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='check if dirty')
+    parser.add_argument('repo_path', metavar='P', type=str, help='path to repository to check')
+    args = parser.parse_args()
 
-	check_if_dirty(args)
+    check_if_dirty(args.repo_path)
