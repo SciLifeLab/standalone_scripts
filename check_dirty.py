ba83b2f18dbefa34cb4955bd0663fd7d566efdda
Alma Hansen <alma.hansen@scilifelab.se>
2023-09-28 13:58:50 +0200

    updated to handle multiple repos

--- check_dirty.py
+++ check_dirty.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-Checks if a repo is dirty.
+Checks if one or several repos are dirty and/or contain untracked files.
 """
 import argparse
 import sys
 from git import Repo
 
-def check_if_dirty(repo_path):
-    repo = Repo(repo_path)
-    if repo.is_dirty():
-        print("Repo is dirty: ", repo_path, file=sys.stderr)
-    if repo.untracked_files:
-        print("Repo has untracked files: ", repo_path, repo.untracked_files, file=sys.stderr)
+def check_if_dirty(repo_paths):
+    for path in repo_paths:
+        repo = Repo(path)
+        if repo.is_dirty():
+            print("Repo is dirty: ", path, file=sys.stderr)
+        if repo.untracked_files:
+            print("Repo has untracked files: ", path, repo.untracked_files, file=sys.stderr)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='check if dirty')
-    parser.add_argument('repo_path', metavar='P', type=str, help='path to repository to check')
+    parser.add_argument('repo_paths', nargs='+', help='one or more paths to repositories to check')
     args = parser.parse_args()
 
-    check_if_dirty(args.repo_path)
+    check_if_dirty(args.repo_paths)
