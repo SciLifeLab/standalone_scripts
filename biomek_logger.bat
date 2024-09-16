bacd4677f5d084d90528a5994a51d1bf7b35120d
Anandashankar Anil <anandasanil@gmail.com>
2024-09-16 15:38:25 +0200

    Update conf file file

--- biomek_logger.bat
+++ biomek_logger.bat
@@ -2,6 +2,6 @@
 SETLOCAL
 SET LOG_PATH="C:\Users\Public\Documents\Biomek5\Logs"
 SET SCRIPT="%USERPROFILE%\Repos\standalone_scripts\upload_biomek_logs.py"
-SET STB_CONF="%USERPROFILE%\Repos\conf\statusdb.yaml"
+SET STB_CONF="%USERPROFILE%\Repos\conf\biomek_upload_conf.yaml"
 SET STB_LOG="%USERPROFILE%\Repos\logs\statusdb_upload.log"
 ECHO %* | C:\Users\Public\miniconda3\envs\biomek_log\python %SCRIPT% --conf %STB_CONF% --logfile %STB_LOG% --log_file_path %LOG_PATH%
